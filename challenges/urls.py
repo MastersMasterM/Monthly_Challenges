cb4c99c503f3d86ef72c96b9291278152afa8646
MastersMasterM <mo.mahdi1379@gmail.com>
2023-04-13 16:24:27 +0430

    Dynamic HTML without Styling

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index),
+    path("",views.index, name = "main"),
     path("<int:month>", views.month_num),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
 
@@ -10,32 +10,32 @@ monthly_challenges = {
     "april": "Whats wrong with me bro!",
     "may" : "Just cry and think about your bad behaviours :))",
     "june": "You are not GOD!!",
-    "july": "Start the loop in july"
+    "july": "Start the loop in july",
+    "agust": "Finishing Django",
+    "september" : None
 }
 
 def index(request):
     mon_name = list(monthly_challenges.keys())
-    response_data = """
-        <ul>
-    """
-    for m in mon_name:
-        ur = reverse("month-challenge",args=[m])
-        response_data = response_data + f"<li><a href={ur}>{m.capitalize()}</a></li>"
-    response_data = response_data + "</ul>"
-    return HttpResponse(response_data)
+    return render(request,"challenges/index.html",{
+        "months": mon_name
+    })
 
-def monthly_challenge(request, month):
+def monthly_challenge(request, month:str):
     
     try:
-        challenge_text = monthly_challenges[month]    
-        return HttpResponse(challenge_text)   
+        challenge_text = monthly_challenges[month]
+        return render(request,"challenges/challenge.html",{
+            "text": challenge_text,
+            "month" : month
+        })   
     except :
-        return HttpResponseNotFound("This month is not supported")
+        raise Http404()
 
     
 def month_num(request, month:int):
     forward_month = list(monthly_challenges.keys())
     if month > len(forward_month):
-        return HttpResponseNotFound("Invalid Month")
+        raise Http404()
     mon = forward_month[month - 1]
     return HttpResponseRedirect(reverse("month-challenge",args=[mon]))
